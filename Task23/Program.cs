bdb5c4a843b645c36e508cec3306e4eeadb0873e
TeifGuru <TeifGuru@gmail.com>
2023-07-18 14:11:15 +0300

    Нахождение кубов

--- Task23/Program.cs
+++ Task23/Program.cs
@@ -1,2 +1,25 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Напишите программу, которая принимает на
+// вход число (N) и выдаёт таблицу кубов чисел от 1 до N.
+// 3 -> 1, 8, 27
+// 5 -> 1, 8, 27, 64, 125
+
+int Promt(string massege)
+{
+    Console.WriteLine(massege);
+    int res=int.Parse(Console.ReadLine()!);
+    return res;
+}
+
+void SQRT(int a)
+{
+    int n=1;
+    while (n<=a)
+    {
+        Console.Write(n*n*n+", ");
+        n=n+1;
+    }
+}
+
+int N=Promt("Введите число:");
+Console.Write(""+N+" -> ");
+SQRT(N);
